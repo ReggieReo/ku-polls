11f0e665030f261011477698ce6f3ee9daea7aab
Setthapon Thadisakun <steam.reaw1@gmail.com>
2023-09-21 13:48:48 +0700

    fix how vote response to user that didn't select a choice

--- polls/views.py
+++ polls/views.py
@@ -96,14 +96,8 @@ def vote(request, question_id):
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(
-            request,
-            "polls/detail.html",
-            {
-                "question": question,
-                "error_message": "You didn't select a choice.",
-            },
-        )
+        messages.error(request, "you didn't select a choice.")
+        return HttpResponseRedirect(reverse("polls:detail", args=[question_id]))
 
     this_user = request.user
     try:
